4085f2895953387f5f4731ef4e26732869e3acbc
Osvaldo <osvaldo.tulini@gmail.com>
2025-02-26 12:14:03 +0100

    refs sparkfabrik-innovation-team/board#3450 - DockerHub new pull limit

--- examples/main.tf
+++ examples/main.tf
@@ -2,8 +2,7 @@
    # A simple example on how to use this module
  */
 module "example" {
-  source  = "github.com/terraform-aws-ecr-pull-through-cache"
-  version = ">= 0.1.0"
+  source = "github.com/terraform-aws-ecr-pull-through-cache?ref=0.1.0"
 
   aws_region = var.aws_region
 }
